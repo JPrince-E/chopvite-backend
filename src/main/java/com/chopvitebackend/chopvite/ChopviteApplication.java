36416516a67ebf347ca0d5a8d74b228b0f4e97a5
JPrince-E <adebayoea1@gmail.com>
2023-11-15 16:42:08 +0100

    Initial commit

---
+++ src/main/java/com/chopvitebackend/chopvite/ChopviteApplication.java
@@ -0,0 +1,13 @@
+package com.chopvitebackend.chopvite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChopviteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChopviteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/chopvitebackend/chopvite/ChopviteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chopvitebackend.chopvite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChopviteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
